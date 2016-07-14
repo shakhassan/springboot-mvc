640d559e1f4412db6f24f5386c10288f79b702ad
rjirwanshah rjhassan <rjirwanshah@rjirwanshah.local>
2016-07-14 13:50:40 +0800

    spring mvc

---
+++ src/test/java/my/winapp/helloworld/SpringbootSpringmvcApplicationTests.java
@@ -0,0 +1,18 @@
+package my.winapp.helloworld;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringbootSpringmvcApplication.class)
+@WebAppConfiguration
+public class SpringbootSpringmvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
